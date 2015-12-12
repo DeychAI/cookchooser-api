8db9e3d4d76a319aa31d8a449075b965e9843efd
Alexander Deych <deigov@gmail.com>
2015-12-12 16:27:34 +0700

    User API

--- v1/User.py
+++ v1/User.py
@@ -1,12 +1,21 @@
-from flask.ext.restful import Resource, abort
+from flask.ext.restful import Resource, abort, fields, marshal, marshal_with
+from flask import g
 
 from auth import auth
 from models import User
 
+user_fields = {
+    'id': fields.Integer,
+    'username': fields.String,
+    'group': fields.String
+}
 class UserAPI(Resource):
     decorators = [auth.login_required]
+    @marshal_with(user_fields)
     def get(self, user_id):
+        if user_id != g.user.id:
+            abort(403)
         user = User.query.filter_by(id=user_id).first()
         if user is None:
             abort(404, message="User not found!")
-        return {'username': user.username}
+        return user
