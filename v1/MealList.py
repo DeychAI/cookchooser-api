bf96a404fed057161069a2972831d8aa5eb11145
Alexander Deych <deych@saritasa.com>
2015-12-13 13:00:54 +0700

    spaces

--- v1/MealList.py
+++ v1/MealList.py
@@ -4,23 +4,23 @@ from auth import auth
 from models import Meal, Category, db
 
 cat_fields = {
-	'id': fields.Integer
+    'id': fields.Integer
 }
 
 meal_fields = {
-	'id': fields.Integer,
-	'name': fields.String,
-	'category_id': fields.Integer
+    'id': fields.Integer,
+    'name': fields.String,
+    'category_id': fields.Integer
 }
 
 class MealListAPI(Resource):
     #decorators = [auth.login_required]
     def __init__(self):
-    	self.post_parser = reqparse.RequestParser()
-        self.post_parser.add_argument('name', required = 'True')
-        self.post_parser.add_argument('group', required = 'True')
-        self.post_parser.add_argument('category_id', required = 'True')
-        self.post_parser.add_argument('client_id', required = 'True')
+        self.post_parser = reqparse.RequestParser()
+        self.post_parser.add_argument('name', required='True')
+        self.post_parser.add_argument('group', required='True')
+        self.post_parser.add_argument('category_id', required='True')
+        self.post_parser.add_argument('client_id', required='True')
 
         self.get_parser = reqparse.RequestParser()
         self.get_parser.add_argument('cat')
@@ -30,10 +30,9 @@ class MealListAPI(Resource):
 
     @marshal_with(meal_fields)
     def get(self):
-    	args = self.get_parser.parse_args()
-    	if args['cat'] is None:
-    		meals = Meal.query.all()
-    	else:
-    		meals = Meal.query.filter_by(category_id=args['cat']).all()
-
+        args = self.get_parser.parse_args()
+        if args['cat'] is None:
+            meals = Meal.query.all()
+        else:
+            meals = Meal.query.filter_by(category_id=args['cat']).all()
         return meals
